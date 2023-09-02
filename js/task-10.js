7cf6f202964ba5eb24a616bbc6ea1f8d5750a9b3
kglanos <130902847+kglanos@users.noreply.github.com>
2023-09-02 19:31:01 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const createBtn = document.querySelector('[data-create');
+const destroyBtn = document.querySelector('[data-destroy');
+const boxesContainter = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', () => {
+  const amount = Number(input.value);
+  createBoxes(amount);
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const initialSize = 30;
+  let boxesHTML = '';
+
+  for (let i = 0; i < amount; i++) {
+    const size = initialSize + i * 10;
+    const color = getRandomHexColor();
+    const box = `<div style="width: ${size}px; height: ${size}px; background-color: ${color};"></div>`;
+    boxesHTML += box;
+  }
+  boxesContainter.innerHTML = boxesHTML;
+}
+function destroyBoxes() {
+  boxesContainter.innerHTML = '';
+}
