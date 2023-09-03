bd946f39a3741cb639cc0c2ac13bf86f531e80b8
kglanos <130902847+kglanos@users.noreply.github.com>
2023-09-03 16:53:11 +0200

    correction

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,9 @@ const textInput = document.querySelector(`#name-input`);
 const textOutput = document.querySelector(`#name-output`);
 
 textInput.addEventListener("input", (event) => {
-    textOutput.textContent = event.currentTarget.value;
-
     if (event.currentTarget.value === "") {
-        textInput.textContent = "Anonymus";
+        textOutput.textContent = "Anonymous";
+    } else {
+        textOutput.textContent = event.currentTarget.value;
     }
 });
